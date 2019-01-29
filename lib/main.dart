0d871233adedc85e36cbf07d89bcfecb722dd18e
govind <govind.ku@mobme.in>
2019-01-29 22:24:59 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair =  WordPair.random();
+    return MaterialApp(
+      title: 'The Basic',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TheBasic'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
